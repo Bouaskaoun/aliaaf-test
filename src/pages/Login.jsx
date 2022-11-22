dd992042ba54698712344d5d7273dff17ebff18c
bouaskaoun <mohammed.bouaskaoun@edu.uca.ma>
2022-11-22 20:50:06 +0100

    add the Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import Helmet from '../components/Helmet/Helmet';
+import { Link } from 'react-router-dom';
+
+import '../styles/login.css'
 
 const Login = () => {
+
   return (
-    <div>Login</div>
+    <Helmet title='Login'>
+      <section>
+        <Container>
+          <Row>
+          <Col lg='6' className='m-auto text-center'>
+            <h3 className='fw-bold mb-4'>Login</h3>
+            <Form className='auth__form'>
+              <FormGroup className='form__group'>
+                <input 
+                  type='email' 
+                  placeholder='Enter your email' 
+                />
+              </FormGroup>
+              <FormGroup className='form__group'>
+                <input 
+                  type='password' 
+                  placeholder='Enter your password' 
+                />
+              </FormGroup>
+              <button type='submit' className='buy__btn auth__btn'>Login</button>
+              <p>Don't have an account? <Link to='/signup'>Create an account</Link></p>
+            </Form>
+          </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 

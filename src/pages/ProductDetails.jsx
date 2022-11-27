02b66fe43e3b1e1578209ade56528a9d9ff6f170
bouaskaoun <mohammed.bouaskaoun@edu.uca.ma>
2022-11-27 01:57:28 +0100

    update the product details jsx to get data from firebase

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,46 +3,61 @@ import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import {motion} from 'framer-motion';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../firbase.config';
+
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/CommonSection';
 import ProductsList from '../components/UI/ProductsList';
 
-import products from '../assets/data/products';
 import '../styles/product-details.css';
 
 
 const ProductDetails = () => {
 
-  const {id} = useParams()
-  const product = products.find(item => item.id === id)
-  const {
-    imgUrl,
-    productName,
-    price,
-    avgRating,
-    description,
-    shortDesc,
-    category
-  } = product
+  const [products, setProducts] = useState([])
 
-  const relatedProducts = products.filter(item => item.category === category)
+  useEffect(() => {
+    async function fetchProducts() {
+        const data = []
+        const querySnapshot = await getDocs(collection(db, "products"));
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            data.push(doc.data());
+        });
+
+        setProducts(data);
+    }
+
+    fetchProducts();
+  }, []);
+
+  const {id} = useParams()
+  console.log(id)
+  const product = products.find(item => item.id === id)
+  
+  const relatedProducts = products.filter(item => item.category === product?.category)
+
+  const addToCart = () => {
+    toast.success('Product added successfully')
+  }
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, [products]);
 
   return (
-    <Helmet title = {productName}>
-      <CommonSection title = {productName} />
+    <Helmet title = {product?.productName}>
+      <CommonSection title = {product?.productName} />
       <section className='pt-0'>
         <Container>
           <Row>
             <Col lg='6'>
-              <img src={imgUrl} alt="" />
+              <img src={product?.imgUrl} alt="" />
             </Col>
             <Col lg='6'>
               <div className="product__details mt-5">
-                <h2>{productName}</h2>
+                <h2>{product?.productName}</h2>
                 <div className="product__rating d-flex align-items-center gap-5 mb-3">
                   <div>
                     <span><i class="ri-star-s-fill"></i></span>
@@ -51,13 +66,13 @@ const ProductDetails = () => {
                     <span><i class="ri-star-s-fill"></i></span>
                     <span><i class="ri-star-half-line"></i></span>
                   </div>
-                  <p>(<span>{avgRating}</span>ratings)</p>
+                  <p>(<span>{product?.avgRating}</span>ratings)</p>
                 </div>
                 <div className="d-flex align-items-center gap-5">
-                  <span className='product__price'>${price}</span>
-                  <span>Category: {category}</span>
+                  <span className='product__price'>${product?.price}</span>
+                  <span>Category: {product?.category}</span>
                 </div>
-                <p className='mt-3'>{shortDesc}</p>
+                <p className='mt-3'>{product?.shortDesc}</p>
                 <motion.button 
                   whileTap={{ scale: 1.2 }}
                   className='buy__btn'
@@ -81,7 +96,7 @@ const ProductDetails = () => {
                 </h6>
               </div>
               <div className="tab__content mt-5">
-                <p>{description}</p>
+                <p>{product?.description}</p>
               </div>
             </Col>
             <Col lg='12' className='mt-5 mb-5'>

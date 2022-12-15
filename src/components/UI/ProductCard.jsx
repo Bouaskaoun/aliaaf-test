0fee306adbaa97d7835f0cd32286e4f2e9ed0ffc
bouaskaoun <mohammed.bouaskaoun@edu.uca.ma>
2022-12-15 20:53:23 +0100

    fix the Link issue

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Col } from 'reactstrap';
 import {motion} from 'framer-motion';
 import '../../styles/product-card.css';
@@ -7,15 +7,18 @@ import '../../styles/product-card.css';
 
 const ProductCard = ({ item }) => {
 
+    const navigate = useNavigate()
+
   return (
     <Col lg='3' md='4' className="mb-2">
         <div className="product__item">
-            <div className="product__img">
+            <div className="product__img" onClick={()=> navigate(`/shop/${item._id}`)}>
                 <motion.img whileHover={{ scale: 0.9 }} src={item.img} alt="" />
             </div>
             <div className="p-2 product__info">
                 <h3 className='product__name'>
-                    <Link to={`/shop/${item._id}`}>{item.title}</Link>
+                    {item.title}
+                    {/* <Link to={`/shop/${item._id}`}>{item.title}</Link> */}
                 </h3>
                 <span>{item.category}</span>
             </div>

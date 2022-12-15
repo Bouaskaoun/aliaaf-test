714fa33cbf0896c3ef55c3107325724be6df541b
bouaskaoun <mohammed.bouaskaoun@edu.uca.ma>
2022-12-15 21:01:37 +0100

    remove Link

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Col } from 'reactstrap';
 import {motion} from 'framer-motion';
 import '../../styles/product-card.css';
